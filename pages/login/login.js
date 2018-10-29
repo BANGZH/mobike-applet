ac341fd2fdef291a5475766068672be2ccdd0ece
BANGZH <446487919@qq.com>
2018-10-30 00:33:30 +0800

    login css modify

--- pages/login/login.js
+++ pages/login/login.js
@@ -10,62 +10,6 @@ Page({
       vercode:null,
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  },
-
   // 获取用户输入的手机号码
   getinputphone: function(e){
       this.data.userphone = e.detail.value;
@@ -77,62 +21,7 @@ Page({
   },
 
   // 获取验证码
-  getcode:function(){
-    var userphone = this.data.userphone;
-
-    wx.request({
-      url: 'http://127.0.0.1:8080/user/login/requestSMS/'+userphone,
-      method:'GET',
-      data:{},
-      success:function(res){
-        console.log("ddd"+res);
-        // console.log(res.data);
-        if(res.data.code == 1){
-            wx.showToast({
-              title: '验证码发送成功',
-            })
-        }
-      }
-    })
-  },
-
-  // 确认登陆验跳转
-  gologin:function(event){
-    var that = this;
-    var userphone = that.data.userphone;
-    var vercode = that.data.vercode;
-    wx.request({
-      url: 'http://127.0.0.1:8080/user/login/' + userphone,
-      method:'POST',
-      data: { sendVerificationCodeStr:vercode},
-      success:function(res){
-        if(res.data.code == 1){
-          wx.setStorageSync("sessionId", res.header["Set-Cookie"].slice(0, -18));
-          wx.showToast({
-            title: '登陆成功',
-          })
-          wx.redirectTo({
-            url: '../index/index?userphone='+userphone,
-          })
-        }
-        else{
-          var tip = res.data.message;
-          wx.showModal({
-            title: '提示',
-            content: tip,
-          })
-        } 
-      }
-    })
-  },
-
-  // 显示用户手机验证码
-  showver:function(){
-    console.log(this.data.userphone+"验证码"+this.data.vercode);
-  },
-
-  
-  getvercode2:function(){
+  getvercode:function(){
       var that = this;
       var userphone = that.data.userphone;
     wx.request({
@@ -157,6 +46,7 @@ Page({
     })
   },
 
+  // 确认登陆跳转
   gologin2: function (event) {
     var that = this;
     var userphone = that.data.userphone;
@@ -185,5 +75,12 @@ Page({
       }
     })
   },
+
+// 跳转至注册界面
+  toRegistered:function(){
+     wx.redirectTo({
+       url: '../registered/registered',
+     })
+  }
 
 })
